676267d6dbdd8ca3ad32ced79d6d4bd0daa7103b
BigHuni <hdh8659@gmail.com>
2024-08-13 00:57:18 +0900

    refactor : null 처리 위한 Optional 반환 타입 수정

--- src/main/java/daehun/trip_java/User/repository/UserRepository.java
+++ src/main/java/daehun/trip_java/User/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package daehun.trip_java.User.repository;
 
 import daehun.trip_java.User.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 }
