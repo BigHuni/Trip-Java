47cbeac3032005eae754b95667f4b80bcd061f21
BigHuni <hdh8659@gmail.com>
2024-08-13 00:01:07 +0900

    refactor : 수정된 시간정보 어노테이션(LastModifiedDate) 추가

--- src/main/java/daehun/trip_java/User/common/commonDate.java
+++ src/main/java/daehun/trip_java/User/common/commonDate.java
@@ -5,6 +5,7 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -13,5 +14,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class commonDate {
   @CreatedDate
   private LocalDateTime created_at;
+  @LastModifiedDate
   private LocalDateTime updated_at;
 }
